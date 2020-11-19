468e7e3792f4c1c1dc023051d693819f14651197
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2020-11-19 14:27:39 -0300

    Ajustado display de produtos dinamicos

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import InfoCards from '../../components/InfoCards';
 
@@ -6,10 +6,12 @@ import api from '../../services';
 import { Container, Product, ProductCategory } from './style';
 
 interface Products {
+  [key: number]: number;
   id: number;
   name: string;
   code?: number;
   minimum_stock: number;
+  category_id: number;
   category: {
     name: string;
   };
@@ -53,64 +55,88 @@ const Products: React.FC = () => {
     return productList;
   }, [products]);
 
-  const panosCategory = useMemo(() => {
-    const productList = classifiedProductList.filter(
-      (product) => product.formattedCategory === 'Panos'
-    );
-    return productList;
-  }, [classifiedProductList]);
+  // const panosCategory = useMemo(() => {
+  //   const productList = classifiedProductList.filter(
+  //     (product) => product.formattedCategory === 'Panos'
+  //   );
+  //   return productList;
+  // }, [classifiedProductList]);
 
-  const officeCategory = useMemo(() => {
-    const productList = classifiedProductList.filter(
-      (product) => product.formattedCategory === 'Material de Escritório'
-    );
-    return productList;
-  }, [classifiedProductList]);
+  const categoryzed = classifiedProductList.reduce(
+    (map: Array<Products[]>, obj) => {
+      if (!map[obj.category_id]) {
+        map[obj.category_id] = [obj];
+      } else {
+        map[obj.category_id].push(obj);
+      }
+
+      return map;
+    },
+    []
+  );
+
+  // console.log(categoryzed);
+  // const categorizedItems = (productList: Products[]) => {
+  //   let categorizedList: Products[];
+  //   productList.map((product) => {
+  //     return categorizedList[product.formattedCategory].push(product);
+  //   });
+  // };
+  // function groupBy2(productList: Products[], category: number) {
+  //   return productList.reduce((acc: Products[], product: Products) => {
+  //     const key = product[category];
+  //     if (!acc[key]) {
+  //       acc[key] = [];
+  //     }
+  //     acc[key].push(product);
+  //     return acc;
+  //   }, {});
+  // }
+  // const groupBy = (productList: Products[]) => {
+  //   return productList.reduce(
+  //     (products: Products[], product: Products, index: number) => (
+  //       (product[category_id(index)] ||= []).push(index), product
+  //     ),
+  //     []);
+  //     }
+  // };
+  // const officeCategory = useMemo(() => {
+  //   const productList = classifiedProductList.filter(
+  //     (product) => product.formattedCategory === 'Material de Escritório'
+  //   );
+  //   return productList;
+  // }, [classifiedProductList]);
 
   return (
     <>
       <div>
         <InfoCards />
       </div>
-      <ProductCategory>Panos</ProductCategory>
-      <Container>
-        {panosCategory &&
-          panosCategory.map((product) => {
-            return (
-              <Product key={product.id} status={product.status}>
-                <strong>
-                  {product.name}
-                  <span>{`[${product?.code}]`}</span>
-                </strong>
-                <div>
-                  <p>
-                    {`${product.stock?.actual_stock} / ${product.minimum_stock}`}
-                  </p>
-                </div>
-              </Product>
-            );
-          })}
-      </Container>
 
-      <ProductCategory>Materiais de Escritório</ProductCategory>
-      <Container>
-        {officeCategory &&
-          officeCategory.map((product) => {
-            return (
-              <Product key={product.id} status={product.status}>
-                <strong>
-                  {product.name}
-                  <span>{`[${product?.code}]`}</span>
-                </strong>
-                <div>
-                  <p>
-                    {`${product.stock?.actual_stock} / ${product.minimum_stock}`}
-                  </p>
-                </div>
-              </Product>
-            );
-          })}
-      </Container>
+      {categoryzed.map((category) => {
+        return (
+          <>
+            <ProductCategory>{category[0].formattedCategory}</ProductCategory>
+            <Container>
+              {category.map((product) => {
+                return (
+                  <Product key={product.id} status={product.status}>
+                    <strong>
+                      {product.name}
+                      <span>{`[${product?.code}]`}</span>
+                    </strong>
+                    <div>
+                      <p>
+                        {`${product.stock?.actual_stock} / ${product.minimum_stock}`}
+                      </p>
+                    </div>
+                  </Product>
+                );
+              })}
+            </Container>
+          </>
+        );
+      })}
     </>
   );
 };
